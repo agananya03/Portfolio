44696201546635b70edc046831cfb2b94b199a4a
agananya03 <agananya54126@gmail.com>
2026-01-13 22:07:34 +0530

    animations add

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -9,14 +9,19 @@ const Header = () => {
         <motion.div initial={{scale: 0}} whileInView={{scale: 1}} transition={{duration: 0.8, type: 'spring', stiffness: 100}}className=''>
           <Image src={assets.profile_img} alt='' className='rounded-full w-32'/>
         </motion.div>  
-          <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'>Hi! I am Ananya Garg <Image src={assets.hand_icon} alt='' className='w-6'/></h3>
-          <h1 className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>Full Stack Developer</h1> 
-          <p className='max-w-2xl mx-auto font-Ovo'>
+          <motion.h3 initial={{y: -20, opacity: 0}} whileInView={{y: 0, opacity: 1}} transition={{duration: 0.6, delay:0.3}}
+          className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'>Hi! I am Ananya Garg <Image src={assets.hand_icon} alt='' className='w-6'/></motion.h3>
+          <motion.h1 initial={{y: -30, opacity: 0}} whileInView={{y: 0, opacity: 1}} transition={{duration: 0.8, delay:0.5}}
+          className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>Full Stack Developer</motion.h1> 
+          <motion.p initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration: 0.6, delay:0.7}}
+          className='max-w-2xl mx-auto font-Ovo'>
             I am an enthusiastic full stack web developer with experience in creating projects. 
-          </p>
+          </motion.p>
           <div className='flex flex-col sm:flex-row items-center gap-4 mt-4 '>
-            <a href="#contact" className='px-10 py-3 border rounded-full border-white bg-black text-white flex items-center gap-2 dark:bg-transparent'>Contact me <Image src={assets.right_arrow_white} alt='' className='w-4'/></a>
-            <a href="/sample-resume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>My Resume <Image src={assets.download_icon} alt='' className='w-4'/></a>
+            <motion.a initial={{y: 30, opacity: 0}} whileInView={{y: 0, opacity: 1}} transition={{duration: 0.6, delay:1}}
+            href="#contact" className='px-10 py-3 border rounded-full border-white bg-black text-white flex items-center gap-2 dark:bg-transparent'>Contact me <Image src={assets.right_arrow_white} alt='' className='w-4'/></motion.a>
+            <motion.a initial={{y: 30, opacity: 0}} whileInView={{y: 0, opacity: 1}} transition={{duration: 0.6, delay:1.2}}
+            href="/sample-resume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>My Resume <Image src={assets.download_icon} alt='' className='w-4'/></motion.a>
           </div>
     </div>
   )
