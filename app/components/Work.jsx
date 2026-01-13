16e25d034e723da89013cc368f5adfd2cffa3af0
agananya03 <agananya54126@gmail.com>
2026-01-13 13:16:36 +0530

    animations add

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -2,7 +2,7 @@ import { assets, workData } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
-const Work = () => {
+const Work = ({isDarkMode}) => {
   return (
     <div id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
       <h4 className='text-center mb-2 text-lg font-Ovo'>
@@ -14,7 +14,7 @@ const Work = () => {
       <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
         Welcome to my Web development Portfolio! Explore a collection of projects showcasing my expertise.
       </p>
-      <div className='grid grid-cols-auto my-10 gap-5'>
+      <div className='grid grid-cols-auto my-10 gap-5 dark:text-black'>
         {workData.map((project, index) => (
           <div key={index} style={{backgroundImage: `url(${project.bgImage})`}} className='aspect-square cursor-pointer bg-no-repeat bg-cover bg-center rounded-lg relative group'>
             <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7'>
@@ -29,7 +29,8 @@ const Work = () => {
           </div>
         ))}
       </div>
-      <a href="" className='flex items-center justify-center w-max gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500'>Show More <Image src={assets.right_arrow_bold} alt='Right arrow' className='w-4'/></a>
+      <a href="" className='flex items-center justify-center w-max gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500 dark:text-white dark:border-white dark:hover:bg-darkHover'>Show More
+        <Image src={isDarkMode ? assets.right_arrow_bold_dark : assets.right_arrow_bold} alt='Right arrow' className='w-4'/></a>
     </div>
   )
 }
