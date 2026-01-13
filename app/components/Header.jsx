50c581010ce89b5b98bf4cede4f8c1c08c7677e8
agananya03 <agananya54126@gmail.com>
2026-01-13 13:15:50 +0530

    animations add

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,21 +1,22 @@
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
+import { motion } from "motion/react"
 
 const Header = () => {
   return (
     <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4'>
-        <div className=''>
+        <motion.div initial={{scale: 0}} whileInView={{scale: 1}} transition={{duration: 0.8, type: 'spring', stiffness: 100}}className=''>
           <Image src={assets.profile_img} alt='' className='rounded-full w-32'/>
-        </div>  
+        </motion.div>  
           <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'>Hi! I am Ananya Garg <Image src={assets.hand_icon} alt='' className='w-6'/></h3>
           <h1 className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>Full Stack Developer</h1> 
           <p className='max-w-2xl mx-auto font-Ovo'>
             I am an enthusiastic full stack web developer with experience in creating projects. 
           </p>
           <div className='flex flex-col sm:flex-row items-center gap-4 mt-4 '>
-            <a href="#contact" className='px-10 py-3 border rounded-full border-white bg-black text-white flex items-center gap-2'>Contact me <Image src={assets.right_arrow_white} alt='' className='w-4'/></a>
-            <a href="/sample-resume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>My Resume <Image src={assets.download_icon} alt='' className='w-4'/></a>
+            <a href="#contact" className='px-10 py-3 border rounded-full border-white bg-black text-white flex items-center gap-2 dark:bg-transparent'>Contact me <Image src={assets.right_arrow_white} alt='' className='w-4'/></a>
+            <a href="/sample-resume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>My Resume <Image src={assets.download_icon} alt='' className='w-4'/></a>
           </div>
     </div>
   )
